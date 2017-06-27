dab1f411ba1be41b9738f5bb55307a9660726bbc
Zhou Bin <zmonoid@gmail.com>
2017-06-27 15:34:54 +0800

    .

--- ensemble.py
+++ ensemble.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import glob
+import argparse
 
-fnames = glob.glob('./logs/*/result*.csv')
-print fnames
-flist = [pd.read_csv(x) for x in fnames]
+parser = argparse.ArgumentParser(description='ensemble')
+parser.add_argument('logs', type=str, help='logs folder prefix')
+args = parser.parse_args()
 
-for item in flist[1:]:
-    flist[0]['invasive'] += item['invasive']
+fnames = glob.glob(args.logs + '*/result*.csv')
+for name in fnames:
+    print name
 
-flist[0]['invasive'] /= len(fnames)
-flist[0].to_csv('ensemble.csv')
+if len(fnames) == 0:
+    print "found no csv"
+else:
+    flist = [pd.read_csv(x) for x in fnames]
+    for item in flist[1:]:
+        flist[0]['invasive'] += item['invasive']
+
+    flist[0]['invasive'] /= len(fnames)
+    flist[0].to_csv('logs/result.csv')
